12ee347ed8bca4461b2f1aa53edf5e953616ba54
LuzTorres <torres.luz.21@hotmail.com>
2017-03-10 21:49:23 -0600

    Add files via upload

---
+++ Tarea3.cpp
@@ -0,0 +1,29 @@
+// Cola y Pila
+class Cola
+{
+public: Nodo H, T;
+	Cola(int dato);
+	Cola();
+	Insertar F();
+	Borar P();
+
+	if (is Vacio());
+	{
+		Buscar();
+	}
+	Buscar();
+};
+
+class Pila
+{
+public: Nodo Tope
+	Pila(int dato);
+	Pila();
+	Pusch(); //InsertarP
+	Pop();   //BorrarP
+	if (is Vacio());
+	{
+		Buscar();
+	}
+	Buscar();
+};
